8f1d6c1c0cb28e4a0e4b2aca1e1b2b7a01e02b1c
Caitlin Ash <caitlinba27@gmail.com>
2024-01-02 21:55:25 -0600

    Added code to main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import './App.css';
+
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+);
