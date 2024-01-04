a0647595153242f48dc9cdbb81307fce8e4950eb
Caitlin Ash <caitlinba27@gmail.com>
2024-01-03 22:44:29 -0600

    Added animations with gsap to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,9 @@ import {
 
 import image from "../assets/IMG-9567.jpeg";
 
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+
 const theme = createTheme({
     palette: {
         primary: {
@@ -28,6 +31,34 @@ const theme = createTheme({
 });
 
 function About() {
+    const paragraphOne = useRef(null);
+    const paragraphTwo = useRef(null);
+    const img = useRef(null);
+
+    useEffect(() => {
+        const tl = gsap.timeline();
+
+        tl.from(img.current, {
+            duration: 1,
+            opacity: 0,
+            y: 100,
+            ease: "power4.out",
+        
+        })
+
+        .from(paragraphOne.current, {
+            duration: 1,
+            opacity: 0,
+            y: 100,
+            ease: "power4.out",
+        })
+            .from(paragraphTwo.current, {
+                duration: 1,
+                opacity: 0,
+                y: 100,
+                ease: "power4.out",
+        })
+    })
     return (
         <ThemeProvider theme={theme}>
             {/* <Box>
@@ -37,15 +68,16 @@ function About() {
             </Box> */}
             <Box className='about-container'>
                 <Box className='about-image-container'>
-                    <img className='about-image' src={image} />
+                    <img className='about-image' ref={img} src={image} />
                 </Box>
                 <Box className='about-text-container'>
-                    <Typography className='about-text'>
+                    <Typography ref={paragraphOne} className='about-text'>
                         I am a passionate full-stack web developer who recently embarked on an exciting journey through a rigorous coding boot camp.
                         My interest in technology and programming has now been armed with my newfound knowledge and programming skills, transforming ideas into engaging web experiences.
                         My journey into the field of web development is unique, with my academic background in Psychology. This background has equipped me with the knowledge of human behavior.
                     </Typography>
-                    <Typography className='about-text'>
+                    <br></br>
+                    <Typography ref={paragraphTwo} className='about-text'>
                         My knowledge and fascination of Pyschology has allowed me to understand user behavior and preferences, which has guided me through my approach to web development and design.
                         I believe it is important to craft intuitive user interfaces while building robust backend systems. My goal is to create web applications that meet these technical standards and resonate with users on a psychological level.
                         Beyond this, I am a life-long learner committed to staying on top of industry trends and navigate through the ever changing world of web development one line of code at a time.
