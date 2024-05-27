ebdff69fc62a4beacd9b68f23946f3716371ba60
Caitlin Ash <caitlinba27@gmail.com>
2024-05-26 20:11:24 -0500

    Added original globals.css file from first nextjs app installation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
-// import '../styles/globals.css';
+import '../styles/globals.css';
 import { GridBackground } from '@/components/ui/GridBackground';
 import SmoothScroll from '@/components/ui/SmoothScroll';
 
