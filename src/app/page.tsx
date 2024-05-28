6876ee24f4acde350aa9e82977f22dd0d8755a1a
Caitlin Ash <caitlinba27@gmail.com>
2024-05-27 21:54:05 -0500

    Added chat component to page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Hero from '../components/Hero';
 import About from '../components/sections/About';
 import Projects from '../components/sections/Projects';
 import Skills from '../components/sections/Skills';
-// import chat
+import Chat from '../components/sections/Chat';
+// import footer
 
 import { useState } from 'react';
 
@@ -38,7 +39,7 @@ export default function Home() {
       <Skills />
     </div>
     {/* add footer component */}
-    {/* add chat component */}
+    {activeSection === 'Chat' && <Chat />}
   </div>
   );
 };
