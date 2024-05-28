6f27db5d1dd0fe60227ed3124fb6a61edff15913
Caitlin Ash <caitlinba27@gmail.com>
2024-05-27 22:09:45 -0500

    Added cache-handler for build and deploy to netlify

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    generateBuildId: async () => {
+        // This could be anything, using the latest git hash
+        return process.env.GIT_HASH || 'default-build-id';
+    },
+    experimental: {
+        cacheHandler: require.resolve('./cache-handler.ts'),
+        cacheMaxMemorySize: 0, // disable default in-memory caching
+    },
+};
+
+export default nextConfig;
