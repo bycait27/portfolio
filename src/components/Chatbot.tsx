13bbea418326c54303875337e285a09bf964433f
Caitlin Ash <caitlinba27@gmail.com>
2024-05-27 21:51:51 -0500

    Added chatbot component to add to chat component as well as styled the chatbot

---
+++ src/components/Chatbot.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import styles from '../styles/chatbot.module.css';
+
+const faqs = [
+    {
+        question: 'Just saying hi :)',
+        answer: 'Hi, Thank you for taking the time to view my portfolio!'
+    },
+    {
+        question: 'How can I contact you?',
+        answer: 'You can contact me at my email: caitlinba27@gmail.com!',
+    },
+    {
+        question: 'What tech stack do you use?',
+        answer: 'I am currently using Typescript, Next.js, Firebase, and GraphQL!'
+    },
+];
+
+export default function Chatbot() {
+    const [conversation, setConversation] = useState([
+        { message: 'Hi!', type: 'bot' },
+        { message: 'What would you like to know?', type: 'bot' }
+    ]);
+    const [clickedQuestions, setClickedQuestions] = useState(new Set());
+
+    const handleQuestionClick = (faq) => {
+        if (clickedQuestions.has(faq.question)) return;
+
+        setConversation((prev) => [
+            ...prev,
+            { message: faq.question, type: 'user' },
+            { message: faq.answer, type: 'bot' }
+        ]);
+
+        setClickedQuestions((prev) => new Set(prev).add(faq.question));
+    };
+
+    useEffect(() => {
+        const chatContainer = document.querySelector(`.${styles.chatContainer}`);
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }, [conversation]);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>FAQ Chat Bot</h1>
+            </div>
+
+            <div className={styles.chatContainer}>
+                <AnimatePresence>
+                    {conversation.map((chat, index) => (
+                        <motion.div
+                            key={index}
+                            className={chat.type === 'bot' ? styles.botMessage : styles.userMessage}
+                            initial={{ opacity: 0, x: 50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: 50 }}
+                            transition={{ duration: 0.5, delay: index * 0.3 }}
+                        >
+                            <p className={styles.message}>{chat.message}</p>
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
+
+                <div className={styles.options}>
+                    {faqs.map((faq, index) => (
+                        <p
+                            key={index}
+                            className={`${styles.question} ${clickedQuestions.has(faq.question) ? styles.disabledQuestion : ''}`}
+                            onClick={() => handleQuestionClick(faq)}
+                        >
+                            {faq.question}
+                        </p>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
