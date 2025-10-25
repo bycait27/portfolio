84e9fd16f2b1d942efb8204390ce23b8d64e11af
bycait27 <caitlinba27@gmail.com>
2025-10-24 21:03:57 -0500

    Update Projects page to emphasize backend/devops

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -4,39 +4,39 @@ import SectionHeader from '../ui/SectionHeader';
 const projectData = [
     {
         projectTitle: 'The Tech Blog',
-        projectDescription: 'A CMS-Style tech blog site for developers following the MVC paradigm.',
+        projectDescription: 'A CMS-style blog site with CI/CD pipeline and Docker containerization.',
         viewCode: 'https://github.com/bycait27/tech-blog',
         viewApp: 'https://the-tech-blog7-f8ee982770ad.herokuapp.com/',
-        tech: ['JavaScript', 'Handlebars.js', 'Sequelize', 'MySQL', 'Express']
+        tech: ['JavaScript', 'Node.js', 'Express', 'MySQL', 'Docker', 'GitHub Actions']
     },
-    {
-        projectTitle: 'Book Search Engine',
-        projectDescription: 'A Google Books GraphQL API built using the MERN stack.',
-        viewCode: 'https://github.com/bycait27/book-search-engine',
-        viewApp: 'https://book-search-engine-caitlins-projects-dc84348c.vercel.app/',
-        tech: ['JavaScript', 'React', 'MongoDB', 'GraphQL', 'Apollo', 'Express']
-    },
-    {
-        projectTitle: 'Text Editor',
-        projectDescription: 'A simple text editor that meets the PWA criteria.',
-        viewCode: 'https://github.com/bycait27/pwa-text-editor',
-        viewApp: 'https://bloodcurdling-beast-50523-57a8116f0051.herokuapp.com/',
-        tech: ['JavaScript', 'HTML', 'CSS', 'Webpack']
-    },
-    {
-        projectTitle: 'Weather Dashboard',
-        projectDescription: 'A web app that allows users to view the current and 5-day weather forecast.',
-        viewCode: 'https://github.com/bycait27/weather-dashboard',
-        viewApp: 'https://weather-dashboard7.netlify.app/',
-        tech: ['JavaScript', 'HTML', 'CSS']
-    },
-    {
-        projectTitle: 'Work Day Scheduler',
-        projectDescription: 'A simple work day scheduler app powered by jQuery to track events.',
-        viewCode: 'https://github.com/bycait27/work-day-scheduler',
-        viewApp: 'https://bycait27.github.io/work-day-scheduler/',
-        tech: ['JavaScript', 'HTML', 'CSS', 'jQuery', 'Day.js']
-    },
+    // {
+    //     projectTitle: 'Book Search Engine',
+    //     projectDescription: 'A Google Books GraphQL API built using the MERN stack.',
+    //     viewCode: 'https://github.com/bycait27/book-search-engine',
+    //     viewApp: 'https://book-search-engine-caitlins-projects-dc84348c.vercel.app/',
+    //     tech: ['JavaScript', 'React', 'MongoDB', 'GraphQL', 'Apollo', 'Express']
+    // },
+    // {
+    //     projectTitle: 'Text Editor',
+    //     projectDescription: 'A simple text editor that meets the PWA criteria.',
+    //     viewCode: 'https://github.com/bycait27/pwa-text-editor',
+    //     viewApp: 'https://bloodcurdling-beast-50523-57a8116f0051.herokuapp.com/',
+    //     tech: ['JavaScript', 'HTML', 'CSS', 'Webpack']
+    // },
+    // {
+    //     projectTitle: 'Weather Dashboard',
+    //     projectDescription: 'A web app that allows users to view the current and 5-day weather forecast.',
+    //     viewCode: 'https://github.com/bycait27/weather-dashboard',
+    //     viewApp: 'https://weather-dashboard7.netlify.app/',
+    //     tech: ['JavaScript', 'HTML', 'CSS']
+    // },
+    // {
+    //     projectTitle: 'Work Day Scheduler',
+    //     projectDescription: 'A simple work day scheduler app powered by jQuery to track events.',
+    //     viewCode: 'https://github.com/bycait27/work-day-scheduler',
+    //     viewApp: 'https://bycait27.github.io/work-day-scheduler/',
+    //     tech: ['JavaScript', 'HTML', 'CSS', 'jQuery', 'Day.js']
+    // },
 ];
 
 export default function Projects() {
@@ -46,22 +46,18 @@ export default function Projects() {
                 {/* section header */}
                 <SectionHeader 
                     title="Projects"
-                    subtitle="Here are a few of my favorite projects!"
+                    subtitle=""
                     emoji="📓"
                 />
-
-                <div className='text-center font-bold pt-20'>
-                    Coming soon...stay tuned!
-                </div>
                 {/* projects grid */}
-                {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
                     {projectData.map((project, index) => (
                         <Project 
                             key={index} 
                             {...project} 
                         />
                     ))}
-                </div> */}
+                </div>
             </div>
         </div>
     );
